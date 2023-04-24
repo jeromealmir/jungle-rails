bf20c824894b51d783ba8e95f6430608c4f23b63
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-04-24 13:52:22 -0700

    undo last push

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,5 @@
 class Admin::ProductsController < ApplicationController
 
-  http_basic_authenticate_with name: ENV['HTTPAUTH_USERNAME'], password: ENV['HTTPAUTH_PASSWORD']
-
   def index
     @products = Product.order(id: :desc).all
   end
