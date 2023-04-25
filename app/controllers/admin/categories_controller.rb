ec607ff2629122bd2c603c75dccf8f49e7da4ba8
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-04-24 20:22:37 -0700

    define category actions

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,10 +1,36 @@
 class Admin::CategoriesController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['HTTPAUTH_USERNAME'], password: ENV['HTTPAUTH_PASSWORD']
+
   def index
+    @categories = Category.order(id: :desc).all
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @category = Category.find params[:id]
+    @category.destroy
+    redirect_to [:admin, :categories], notice: 'Product deleted!'
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+    )
   end
 end
